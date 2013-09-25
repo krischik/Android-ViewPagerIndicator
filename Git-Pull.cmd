74247beae15b02e405676844222beaa8c5afd1b5
Martin Krischik <martin.krischik@noser.com>
2013-09-25 11:33:32 +0200

    new version.

--- Git-Pull.cmd
+++ Git-Pull.cmd
@@ -14,17 +14,16 @@ SETLOCAL
     SET PATH=%PATH%;C:\opt\Git\bin
     SET PATH=%PATH%;C:\opt\Scala\2.10.0\bin
 
-    CALL scala -language:postfixOps -save %~f0 %*    
+    CALL scala -language:postfixOps -save %~f0 %*
 ENDLOCAL
-    
+
 GOTO :eof
 ::!#
 
 import scala.sys.process._
 
-"git" :: "pull" :: "https://github.com/JakeWharton/Android-ViewPagerIndicator.git"  :: Nil !;
-"git" :: "pull" :: Nil !;
-"git" :: "push" :: Nil !;
+("git" :: "pull" :: "https://github.com/JakeWharton/Android-ViewPagerIndicator.git"  :: Nil).!
+("git" :: "pull" :: "origin" :: "dev" :: Nil).!
 
 // vim: set wrap tabstop=8 shiftwidth=4 softtabstop=4 noexpandtab :
 // vim: set textwidth=0 filetype=scala foldmethod=marker nospell :
